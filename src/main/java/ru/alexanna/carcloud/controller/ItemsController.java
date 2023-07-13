7e9c7b675fd67629d187443bb87c1089b978a66c
Oleg Shuljakov <procundo@yandex.ru>
2023-07-13 08:31:27 +0300

    12.07.23 home update.

--- src/main/java/ru/alexanna/carcloud/controller/ItemsController.java
+++ src/main/java/ru/alexanna/carcloud/controller/ItemsController.java
@@ -14,42 +14,43 @@ import java.util.stream.Collectors;
 
 @RestController
 //@CrossOrigin(origins = {"http://carcloud:8189"})
+@RequestMapping(path = "api/v1", produces = "application/json")
 @RequiredArgsConstructor
 public class ItemsController {
     private final ItemService itemService;
     private final MappingUtils mappingUtils;
 
-    @GetMapping("api/v1/items")
+    @GetMapping("/items")
     public Iterable<ItemDto> findAllItems() {
         return itemService.findAllItems().stream().map(mappingUtils::mapToItemDto)
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("api/v1/item/{id}")
+    @GetMapping("/item/{id}")
     public ItemDto findItemById(@PathVariable Long id) {
         Item item = itemService.findItem(id);
         return mappingUtils.mapToItemDto(item);
     }
 
-    @PostMapping(path = "api/v1/items", consumes = "application/json")
+    @PostMapping(path = "/items", consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public ItemDto createNewItem(@RequestBody ItemDto itemDto) {
         itemDto.setConnectionState(false);
         try {
-            Item item = itemService.createNewItem(itemDto);
-            return mappingUtils.mapToItemDto(item);
+            Item newItem = itemService.createNewItem(itemDto);
+            return mappingUtils.mapToItemDto(newItem);
         } catch (RuntimeException e) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
         }
     }
 
-    @DeleteMapping(path = "api/v1/item/{id}")
+    @DeleteMapping(path = "/item/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteItem(@PathVariable Long id) {
         try {
             itemService.deleteItem(id);
         } catch (EmptyResultDataAccessException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Entry not deleted because missing");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Item not deleted because missing");
         }
     }
 }
--- src/main/java/ru/alexanna/carcloud/controller/MessagesController.java
+++ src/main/java/ru/alexanna/carcloud/controller/MessagesController.java
@@ -1,26 +1,24 @@
 package ru.alexanna.carcloud.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.alexanna.carcloud.dto.MonitoringPackage;
 import ru.alexanna.carcloud.service.services.TerminalMessageService;
 
 
 @RestController
+@RequestMapping(path = "api/v1/messages", produces = "application/json")
 @RequiredArgsConstructor
 public class MessagesController {
     private final TerminalMessageService terminalMessageService;
 // FIXME: Маппинг в DTO (MonitoringPackage) делать, наверное, тут
-    @GetMapping("api/v1/messages/load-last-hour/{id}")
-    public Iterable<MonitoringPackage> loadMessagesLastHour(@PathVariable Long id) {
-        return terminalMessageService.findTerminalMessagesLastHour(id);
+    @GetMapping("/load-last-hour/{itemId}")
+    public Iterable<MonitoringPackage> loadMessagesLastHour(@PathVariable Long itemId) {
+        return terminalMessageService.findTerminalMessagesLastHour(itemId);
     }
 
-    @GetMapping("api/v1/messages/{id}")
-    public Iterable<MonitoringPackage> loadMessagesInterval(@PathVariable Long id, @RequestParam Long timeFrom, @RequestParam Long timeTo) {
-        return terminalMessageService.findTerminalMessages(id, timeFrom, timeTo);
+    @GetMapping("/{itemId}")
+    public Iterable<MonitoringPackage> loadMessagesInterval(@PathVariable Long itemId, @RequestParam Long timeFrom, @RequestParam Long timeTo) {
+        return terminalMessageService.findTerminalMessages(itemId, timeFrom, timeTo);
     }
 }
